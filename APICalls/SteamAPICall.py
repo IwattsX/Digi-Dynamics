5a6252e508bcbdf0edc0c0488a9e434e5760e34b
Isaac Watts <Isaac.s.watts@gmail.com>
2024-01-20 20:04:58 -0500

    Added the sample way of how to get a game using a search_game qiery

--- APICalls/SteamAPICall.py
+++ APICalls/SteamAPICall.py
@@ -1,5 +1,5 @@
 # Use a .env file to use this
-# python SteamAPICall.py
+# python SteamAPICall.py > fileName.txt is how I ran it
 
 # Getting the steam web API : https://steamcommunity.com/dev 
 # Link to API documentation: https://pypi.org/project/python-steam-api/
@@ -12,4 +12,8 @@ from decouple import config
 KEY = config("STEAM_API_KEY")
 steam = Steam(KEY)
 
-print(steam.users.search_user("the12thchairman"))
+# print(steam.users.search_user("the12thchairman"))
+print(steam.apps.search_games("terr")) 
+
+#This helped get the data and put it in a file. This is a Terraria example.
+# NOTE: You can use part of the game names to get the data for all the games that have that prefix
