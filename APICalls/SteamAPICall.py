d54cb0339fbc5302cf0ff6a74802cbb17fa2e0e7
Isaac Watts <Isaac.s.watts@gmail.com>
2024-01-22 12:20:33 -0500

    Modified SteamAPICall for Json dumps as well

--- APICalls/SteamAPICall.py
+++ APICalls/SteamAPICall.py
@@ -7,13 +7,17 @@
 
 from steam import Steam
 from decouple import config
+import json
+
 
 # Getting key from the .env file
 KEY = config("STEAM_API_KEY")
 steam = Steam(KEY)
 
 # print(steam.users.search_user("the12thchairman"))
-print(steam.apps.search_games("terr")) 
+games = steam.apps.search_games("terr")
+with open("Terr.json", 'w') as outfile:
+    json.dump(games, outfile, sort_keys=True, indent=4)
 
 #This helped get the data and put it in a file. This is a Terraria example.
 # NOTE: You can use part of the game names to get the data for all the games that have that prefix
