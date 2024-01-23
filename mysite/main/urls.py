231ac89e1f98ab7930323cfa9ff29b25045eb695
Isaac Watts <Isaac.s.watts@gmail.com>
2024-01-23 17:59:43 -0500

    Added view one, saw how files work together

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("v1/", views.v1, name="view 1"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse("<p>Hello World!</p>")
+
+def v1(response):
+    return HttpResponse("<h1>view 1!</h1>")
