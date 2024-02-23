5878ecb4f60a9533ecc5304ae71b674b2d4bd529
Isaac Watts <Isaac.s.watts@gmail.com>
2024-02-23 14:37:54 -0500

    Modified settings.py with /static/ instead of static/

--- mysite/main/templates/static/js/base.js
+++ mysite/main/templates/static/js/base.js
@@ -1,4 +1,4 @@
 //Base function for a call check
 function myFunction() {
-    alert("Hello from a static file!");
+    alert("Hello changes!");
 }
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "main" / "templates" / "static"]  # new
 
 STATIC_ROOT = BASE_DIR/ "staticfiles"
