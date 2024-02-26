2d5102b3488e1225353762fb540fa695815c766e
Hunter Smith <huntertsmith4778@gmail.com>
2024-02-26 16:44:34 -0500

    styling and playing with JS

--- mysite/main/templates/static/js/base.js
+++ mysite/main/templates/static/js/base.js
@@ -1,4 +1,27 @@
+
+
 //Base function for a call check
 function myFunction() {
-    alert("Hello changes!");
+    alert("Hello change!");
+}
+
+//navbar
+
+//navbar variable
+let home = document.getElementById("home");
+let games = document.getElementById("games");
+let dlc = document.getElementById("dlc");
+let demo = document.getElementById("demo");
+let music = document.getElementById("music");
+let user = document.getElementById("user");
+
+//On Home Click
+function homeBtn(){
+    home.classList.add("font-effect-fire");
+}
+ 
+//On Title Click
+function titleChange(){
+    alert("hi");
+    document.getElementById("title").classList.add("font-effect-fire");
 }
