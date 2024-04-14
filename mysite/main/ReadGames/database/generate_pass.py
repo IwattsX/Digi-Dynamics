09e0da26c0a0fd60091f7f1e2bc8b1fe63ea4b15
Isaac Watts <Isaac.s.watts@gmail.com>
2024-04-13 20:20:10 -0400

    Added a way to generate pass hashes for the users table

---
+++ mysite/main/ReadGames/database/generate_pass.py
@@ -0,0 +1,22 @@
+from Connect_DB import connect, close_connection
+
+def gen_pass(password):
+    if password is None or not isinstance(password, str):
+        return None
+    cnx = connect()
+    cursor = cnx.cursor()
+    query = "SELECT SHA2(%s, 256);"
+    
+    cursor.execute(query, (password,))
+    
+    res = cursor.fetchone()
+
+    close_connection(cursor=cursor, connection=cnx)
+
+    return res
+
+if __name__ == '__main__':
+    test = ['qwerty', '1234', 0, 12.2, '', None]
+    for t in test:
+        print(t)
+        print(gen_pass(t))
