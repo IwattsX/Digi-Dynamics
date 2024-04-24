dbd3b94b37b27fb41d9163e6a7e9ca377aa81d92
Isaac Watts <Isaac.s.watts@gmail.com>
2024-04-24 12:23:08 -0400

    Added Demo data to the table

--- mysite/main/ReadGames/database/InsertIntoSteam.py
+++ mysite/main/ReadGames/database/InsertIntoSteam.py
@@ -331,5 +331,88 @@ class Music():
 
 
 class Demo():
-    def __init__(self):
-        pass
+    def __init__(self, id : str, name : str, support_info : str, Developer : str, Publisher,
+                 Genre : str, Release_Date : str, Required_age : int, Controller_support : str, Website : str, Short_desc : str,
+                 Detailed_desc : str, Supported_languages : str, windows : bool, linux : bool, mac : bool, Header_image : str, Fullgame_id : str):
+        
+        self.Insert_dict = {
+            'id': id,
+            'Name': name,
+            'support_info': support_info if support_info != '' else None,
+            'Developer' : Developer,
+            'Publisher' : Publisher,
+            'Genre' : Genre,
+            'Release_Date' : Release_Date,
+            'Required_age' : Required_age,
+            'Controller_Support' : Controller_support,
+            'Website' : Website,
+            'Short_description' : Short_desc,
+            'Detailed_description': Detailed_desc,
+            'Supported_languages': Supported_languages,
+            'windows': windows,
+            'linux': linux,
+            'mac': mac,
+            'Header_image': Header_image,
+            "Fullgame_id" : Fullgame_id,
+        }
+
+
+        # Explicitly list the columns in the INSERT statement
+        self.columns = ["id", "Name", "support_info", "Developer", "Publisher",
+                        "Genre","Release_Date", "Required_age", "Controller_Support", "Website",
+                        "Short_description", "Detailed_description", "Supported_languages", "windows", "linux", "mac", "Header_image", "Fullgame_id"]
+
+        self.insert()
+    
+    def insert(self):
+
+        connection = connect()
+        cursor = connection.cursor()
+
+        if inDB(cursor, "Games", {'id' : self.Insert_dict["Fullgame_id"]}):
+            print("Foreign key is in the database games")
+            print(self.Insert_dict["Fullgame_id"])
+        else:
+            # print("Fullgame is not in the Games database")
+            close_connection(connection=connection, cursor=cursor)
+            return
+        
+
+        if inDB(cursor, "Demo", self.Insert_dict):
+            print("Already inserted into the table")
+            close_connection(connection=connection, cursor=cursor)
+            return
+        
+
+        Insert_query = f"""
+        INSERT INTO Demo 
+        ({', '.join(self.columns)}) 
+        VALUES
+        (
+        %(id)s,
+        %(Name)s,
+        %(support_info)s,
+        %(Developer)s,
+        %(Publisher)s,
+        %(Genre)s,
+        %(Release_Date)s,
+        %(Required_age)s,
+        %(Controller_Support)s,
+        %(Website)s,
+        %(Short_description)s,
+        %(Detailed_description)s,
+        %(Supported_languages)s,
+        %(windows)s,
+        %(linux)s,
+        %(mac)s,
+        %(Header_image)s,
+        %(Fullgame_id)s
+        );
+        """
+
+        print(Insert_query)
+
+        cursor.execute(Insert_query, self.Insert_dict)
+        connection.commit()  # Commit changes to the database
+
+        close_connection(connection=connection, cursor=cursor)
