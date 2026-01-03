2fe38cbf6e61798dd0b48b0f84fac23f0b5bce6a
Isaac Watts <Isaac.s.watts@gmail.com>
2026-01-03 15:08:45 -0500

    Modified to use uv instead

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.base, name="base"),
-    
-    path("home/", views.home, name="home" ),
-    path("games/", views.games, name="games" ),
+    path("home/", views.home, name="home"),
+    path("games/", views.games, name="games"),
     path("DLC/", views.dlc_view, name="DLC"),
     path("music/", views.music, name="music"),
     path("demo/", views.demo, name="demo"),
